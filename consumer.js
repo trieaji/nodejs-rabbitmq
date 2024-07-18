c5d943fed66896c317d261a0ee3e8b191fbdbc75
Laksa Bayu Trie Aji <laksabayu18@gmail.com>
2024-07-18 14:10:53 +0700

    consumer & producer rabbitmq

---
+++ consumer.js
@@ -0,0 +1,15 @@
+import { connect } from "amqplib";
+
+const connection = await connect("amqp://guest:guest@localhost:5672/");
+const channel = await connection.createChannel();
+
+//membuat consumer
+await channel.consume("sms", function (message) {
+    console.info(message.fields.routingKey)
+    console.info(message.content.toString())
+}, {
+    noAck: true
+})
+
+// await channel.close();
+// await connection.close();
